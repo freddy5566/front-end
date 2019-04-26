6b928a1268fc520cdd853e182bc440dd88484684
Jamfly <cocoredju222@gmail.com>
2019-04-26 11:44:05 +0800

    setup card

---
+++ homework/week6/my_hub/components/Card.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+    <v-card />
+    <a href="">{{ repo }}</a>
+    <div class="language">
+      <span class="language__dot" />
+      <p class="language__text">
+        swift
+      </p>
+      <i class="fas fa-star start" />
+      <p class="language__text">
+        10000
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    repo: {
+      type: String,
+      require: true,
+      default: 'hahaha'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  background-color: white;
+  width: 300px;
+  height: 100px;
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.language {
+  display: flex;
+  margin-top: 40px;
+}
+
+.language__text {
+  display: flex;
+  border: 1px solid black;
+  height: 20px;
+  padding: 0;
+  margin-left: 2px;
+  margin-right: 5px;
+}
+
+.language__dot {
+  height: 10px;
+  width: 10px;
+  background-color: orange;
+  border-radius: 50%;
+  border: 1px solid black;
+  margin-top: 5px;
+}
+
+.start {
+  border: 1px solid black;
+  padding: 0px;
+  height: 20px;
+  margin-left: 2px;
+}
+</style>
--- homework/week6/my_hub/pages/index.vue
+++ homework/week6/my_hub/pages/index.vue
@@ -1,22 +1,38 @@
 <template>
-  <div class="profile">
-    <profileColumn />
+  <div class="grid-box">
+    <div class="profile">
+      <profileColumn />
+    </div>
+    <card
+      class="card"
+      repo="cs193p-2017-fall-demo-and-solution"
+    />
   </div>
 </template>
 
 <script>
 
 import ProfileColumn from '@/components/ProfileColumn.vue'
+import Card from '@/components/Card.vue'
 
 export default {
   components: {
-    ProfileColumn
+    ProfileColumn,
+    Card
   }
 }
 </script>
 
 <style scoped>
+.grid-box {
+  display: grid;
+  grid-template-columns: 20% 80%;
+}
 .profile {
   margin: 100px;
+}
+
+.card {
+  margin: 100px 10px 10px 10px;
 }
 </style>
