46e6eda80bcd593ec0085dc911addeee5acf6b29
Jamfly <cocoredju222@gmail.com>
2019-05-01 15:56:51 +0800

    Fix path issue

--- homework/week6/my_hub/nuxt.config.js
+++ homework/week6/my_hub/nuxt.config.js
@@ -3,7 +3,7 @@ import pkg from './package'
 
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/front-end/week6/'
+    base: '/front-end/homework/week6/'
   }
 } : {}
 
